9bc77ff7673a54f3bb5819cb55fc20e4e54150a2
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-11-01 16:33:25 -0300

    Create pools route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,47 @@
 import Fastify from "fastify";
-import {PrismaClient} from '@prisma/client'
-import cors from '@fastify/cors'
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
-    log:['query'],
-})
+  log: ["query"],
+});
 
-async function bootstrap(){
-    const fastify = Fastify({
-        logger: true,
-    })
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  });
 
-    await fastify.register(cors, {
-        origin: true,
-    })
+  await fastify.register(cors, {
+    origin: true,
+  });
 
-    fastify.get('/pools/count', async () => {
-        const count = await prisma.pool.count()
-        
-        return {count}
-    })
-     " Enable host config on mobile step"
-    await fastify.listen({port:3333, /*host:'0.0.0.0'*/})
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    return reply.status(201).send({ code });
+  });
+
+  await fastify.listen({ port: 3333 /*host:'0.0.0.0'*/ });
 }
 
-bootstrap()
+bootstrap();
