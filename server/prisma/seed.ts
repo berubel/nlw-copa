5f29ffb6ff382e625516084b7427152183c67bee
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-11-01 15:28:10 -0300

    Create prisma seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'Gabriele das Virgens',
+            email: 'gabriele@gmail.com',
+            avatarUrl: 'https://github.com/berubel.png',
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-01T17:50:24.694Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR',
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-01T12:00:24.694Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        },
+    })
+}
+
+main()
