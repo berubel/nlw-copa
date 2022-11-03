ee4c3bea35943b98448e85bce7876e4a974675f7
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-11-03 00:45:23 -0300

    Create authentication interface

--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -1,10 +1,10 @@
-import  {Center} from 'native-base';
-import Logo from '../assets/logo.svg';
+import  {Center, Spinner} from 'native-base';
+
 
 export function Loading() {
     return(
         <Center flex={1} bg="gray.900">
-            <Logo width={212} height={40} />
+            <Spinner color="yellow.500"/>
         </Center>
     )
 }
