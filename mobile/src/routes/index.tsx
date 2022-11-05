6d3075ffc9627f6d8347616b557eedb838654961
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-11-05 12:17:31 -0300

    Redirect user to the app routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,12 +1,17 @@
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
+import { AppRoutes } from './app.routes'
+import { SignIn } from '../screens/SignIn'
+import { useAuth } from '../hooks/useAuth'
+import { Box } from 'native-base'
 
-import {AppRoutes} from './app.routes'
-import {SignIn } from '../screens/SignIn'
-
-export function Routes(){
+export function Routes() {
+    const { user } = useAuth();
     return (
-        <NavigationContainer>
-            <SignIn/>
-        </NavigationContainer>
+        <Box flex={1} bg="gray.900">
+            <NavigationContainer>
+                {user.name ? <AppRoutes /> : <SignIn />}
+            </NavigationContainer>
+        </Box>
+
     )
 }
